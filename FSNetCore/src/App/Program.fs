6f0db4cff6e047f782bb76f6348d257bfcd7242f
andras zakupszki <andras.zakupszki@gmail.com>
2020-04-30 21:38:58 +0100

    First commit

---
+++ FSNetCore/src/App/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// Tutorial and commands from: https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/september/fsharp-do-it-all-with-fsharp-on-net-core 
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments! Here's what JSON>NET has to say about them: "
+    argv
+    |> Array.map Library.getJsonNetJson
+    |> Array.iter (printfn "%s")
+    0 // return an integer exit code
---
+++ FSNetCore/tests/LibTests/Tests.fs
@@ -0,0 +1,10 @@
+module Tests
+
+open System
+open Xunit
+
+[<Fact>]
+let ``Test Hello`` () =
+    let expected = """I used to be Hello but now I'm "Hello" thanks to JSON.NET!"""
+    let actual = Library.getJsonNetJson "Hello"
+    Assert.Equal(expected, actual)
